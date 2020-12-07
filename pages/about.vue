7ad8d1192e5917347e7c6ee243baab284e5af883
alexwelcing <alexwelcing@gmail.coM>
2020-12-07 15:30:03 -0500

    edit site config

--- pages/about.vue
+++ pages/about.vue
@@ -18,15 +18,12 @@
             Quotes are not sourced from all markets and may be delayed up to 20 minutes. Information is provided 'as is' and solely for informational purposes, not for trading purposes or advice.
           </p>
 
-          <h3>Another Fake Heading</h3>
+          <h3>This is in beta access.</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-            ratione rem repudiandae sit!
+            Our service will extend to paid monthly newsletter. Not going to run on medium or substack because we know how to publish without one?
           </p>
           <quote-card />
-          <h3>Still More Fake Content</h3>
+ <!--         <h3>Still More Fake Content</h3>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
             eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
@@ -36,7 +33,7 @@
             <strong>Alias aut facere harum ipsam</strong> quibusdam quidem
             quisquam saepe? Alias architecto blanditiis consequatur consequuntur
             dicta ipsa iste modi provident, quod, similique veritatis.
-          </p>
+          </p> -->
         </div>
 
         <div class="tile is-ancestor">
